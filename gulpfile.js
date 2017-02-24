e56e78e78affe07c17dc25d06ae928f8268d3f0e
cowandp <cowandp@gmail.com>
2017-02-23 21:20:32 -0600

    split polyfills to separate files and concat

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,27 @@ var concat = require("gulp-concat");
 var rename = require("gulp-rename");
 var uglify = require("gulp-uglify");
 
+var inputs = ["./bravo.js", "./lib/*.js"];
+
 gulp.task("dist", function () {
-    return gulp.src("./bravo.js")
+    return gulp.src(inputs)
+        .pipe(concat("bravo.js"))
         .pipe(gulp.dest("dist"))
         .pipe(rename("bravo.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest("dist"));
+});
+
+gulp.task('watch', function () {
+    return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+        .watch(inputs, ['dist'])
+        // When there is a change,
+        // log a message in the console
+        .on('change', function (event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
 });
 
 gulp.task("default", ["dist"]);
